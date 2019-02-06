93bd85eeafe67523f65205aa02ee55442b255410
Nancy <lexyfeer@hotmail.com>
2019-02-05 21:09:19 -0600

    En el apartado src/cipher.js modifique la formula de encode y la de decode la deje como ya la tenia anteriormente dado que esa ya funcionaba, ahora con estos nuevos cambios ambas funcionan y ya pasan los test al 100% solo de este apartado

--- src/cipher.js
+++ src/cipher.js
@@ -1,29 +1,34 @@
 window.cipher = {
-  
-  const encode = (offset,texto) => {
-    console.log('entro'); 
-    let nuevaFrase = '';
-    for(let i=0; i<texto.length;i++) {
-        let nuevoAscii = ((texto.charCodeAt(i) - 65 + offset) % 26) + 65;
-        let letraNueva = String.fromCharCode(nuevoAscii);
-        nuevaFrase = nuevaFrase + letraNueva;
-        //nuevaFrase += letraNueva
-        console.log(nuevaFrase);
-    };
-    return nuevaFrase;
-      
-};
 
-  const decode = (offset, texto) => {
-    let nuevaFrase = '';
-    for(let i=0; i<texto.length;i++) {
-        let nuevoAscii = (texto.charCodeAt(i) + 65 - offset) % 26 + 65;
-        let letraNueva = String.fromCharCode(nuevoAscii);
-        nuevaFrase = nuevaFrase + letraNueva;
-        //nuevaFrase += letraNueva
-    };
-    return console.log(nuevaFrase);
-      
+    encode: (offset,texto) => { 
+        let nuevaFrase = '';
+        for( let i=0; i<texto.length; i++){
+            let letraInicial = texto.charCodeAt(i);
+            //console.log(letraInicial);
+            let iPosicion = letraInicial - 65;
+            //console.log(iPosicion);
+            let nPosicion = iPosicion + offset;
+            //console.log(nPosicion);
+            let pCero = nPosicion %26;
+            //console.log(pCero);
+            let vueltaCompleta = pCero + 65;
+            //console.log(vueltaCompleta);
+            let letraNueva = String.fromCharCode(vueltaCompleta);
+            //console.log(letraNueva);
+            nuevaFrase += letraNueva;
+        }
+        return nuevaFrase;
+    },
+    decode: (offset,texto) => {
+        let fraseDescifrada = '';
+        for(let i=0; i<texto.length;i++) {
+            let nuevoAscii = (texto.charCodeAt(i) + 65 - offset) % 26 + 65;
+            let letraNueva = String.fromCharCode(nuevoAscii);
+            fraseDescifrada = fraseDescifrada + letraNueva;
+            //nuevaFrase += letraNueva
+        }
+        return fraseDescifrada;
+          
+    },
 };
-  // ... 
-};
+  
