f48cd417fb41f51ea0c600ad6834201a564949c9
Nancy <lexyfeer@hotmail.com>
2019-02-06 18:36:31 -0600

    Agregue un input para que muestre el resultado y unos botones para que regresen al inicio o mande una frase de despedida

--- README.md
+++ README.md
@@ -281,19 +281,19 @@ Esta sección está para ayudarte a llevar un control de lo que vas completando.
   el producto.
 * [ ] `README.md` explica claramente cómo el producto soluciona los
   problemas/necesidades de los usuarios.
-* [ ] Usa VanillaJS.
-* [ ] No utiliza `this`.
-* [ ] Implementa `cipher.encode`.
-* [ ] Implementa `cipher.decode`.
+* [X] Usa VanillaJS.
+* [X] No utiliza `this`.
+* [X] Implementa `cipher.encode`.
+* [X] Implementa `cipher.decode`.
 * [ ] Pasa linter con configuración provista.
 * [ ] Pasa pruebas unitarias.
 * [ ] Pruebas unitarias cubren 70% de _statements_, _functions_ y _lines_, y un
   mínimo del 50% de _branches_.
-* [ ] Interfaz permite elegir el `offset` o _desplazamiento_ a usar en el
+* [X] Interfaz permite elegir el `offset` o _desplazamiento_ a usar en el
   cifrado/descifrado.
-* [ ] Interfaz permite escribir un texto para ser cifrado.
+* [X] Interfaz permite escribir un texto para ser cifrado.
 * [ ] Interfaz muestra el resultado del cifrado correctamente.
-* [ ] Interfaz permite escribir un texto para ser descifrado.
+* [X] Interfaz permite escribir un texto para ser descifrado.
 * [ ] Interfaz muestra el resultado del descifrado correctamente.
 
 ### Parte Opcional: "Hacker edition"
--- src/index.js
+++ src/index.js
@@ -1,38 +1,34 @@
 
-
-//function cifrar() {
-//    let offset = document.getElementById("saltos").value;
-//    let textOriginal = document.getElementById('texto').value;
-//    //return console.log(offset,textOriginal);
-//    console.log(window.cipher.encode(offset, textOriginal)); 
-//    
-//    
-//}
-
-//function descifrar() {
-//    let offset = document.getElementById("saltos").value;
-//    let textOriginal = document.getElementById('texto').value;
-//    // return console.log(offset,textOriginal);
-//    console.log(window.cipher.decode(offset, textOriginal)); 
-//}
-
-
-
 let btnCifrar = document.getElementById('btnEncode');
 btnCifrar.addEventListener("click",function () {
     let offset = parseInt(document.getElementById('saltos').value);
     let textOriginal = document.getElementById('texto').value.toUpperCase();
-     console.log(cipher.encode(offset, textOriginal));
-
-    //document.getElementById("demo").innerHTML = window.cipher.encode(offset, textOriginal)
+    let fraseCifrada = (cipher.encode(parseInt(offset), textOriginal));document.getElementById("msjSalida").value = fraseCifrada;
 });
 
 let btnDescifrar = document.getElementById('btnDecode');
 btnDescifrar.addEventListener("click",function () {
     let offset = parseInt(document.getElementById("saltos").value);
     let textOriginal = document.getElementById('texto').value.toUpperCase();
-    console.log(cipher.decode(offset, textOriginal));
-     
-
-    //document.getElementById("demo").innerHTML = window.cipher.decode(offset, textOriginal)
+    let fraseDescifrada = (cipher.decode(parseInt(offset), textOriginal));
+    document.getElementById("msjSalida").value = fraseDescifrada;
 });
+
+//function inicio () {
+//    
+//}
+//let btnSi = document.getElementById("si");
+//btnSi.addEventListener("click", function () {
+//});
+
+function despedida () {
+    let bye = '¡ Hasta la proxima !';
+    document.getElementById("adios").value = bye;
+    
+
+}
+let btnNo = document.getElementById("no");
+btnNo.addEventListener("click", function () {
+    
+});
+
